c9753fe5ba5d835c9cf39fbb3843f66af03dc34c
unknown <oms.otavio@gmail.com>
2023-02-02 00:47:59 -0300

    v1.0

--- LojaDeGames/src/main/java/org/loja/games/repository/ProdutoRepository.java
+++ LojaDeGames/src/main/java/org/loja/games/repository/ProdutoRepository.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.loja.games.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	public List<Produto> findAllByTituloContainingIgnoreCase(String titulo);
 	// busca tudo que estiver dentro da variavel
